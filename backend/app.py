3d44a64127a661aec283108baaf8daf88f4333fd
Shruti Mandrekar <shruti.d.mandrekar@gmail.com>
2025-10-30 17:23:28 -0400

    added CAS login feature

--- backend/app.py
+++ backend/app.py
@@ -1,11 +1,27 @@
 import flask
-# import database
+import flask_session
+import flask_sqlalchemy
+import os
+import auth
+from top import app
 
-app = flask.Flask(__name__, template_folder='../frontend/templates')
+# import database
+session_database_url = os.getenv('SESSION_DATABASE_URL',
+    'sqlite:///proto_sessions.sqlite') 
+session_database_url = session_database_url.replace(
+    'postgres://', 'postgresql://')
+app.config['SESSION_PERMANENT'] = False
+app.config['SESSION_TYPE'] = 'sqlalchemy'
+app.config['SQLALCHEMY_DATABASE_URI'] = session_database_url
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SESSION_SQLALCHEMY'] = flask_sqlalchemy.SQLAlchemy(app)
+flask_session.Session(app)
 
 # Home Page
-@app.route('/', methods=['GET'])
+@app.route('/home', methods=['GET'])
 def home_page():
+    # Protected entrance to page
+    auth.authenticate()
     # NOTE: We probably want a list of dicts here
     # restaurants = database.get_restaurant_cards()
     restaurants = [True, []]  # Placeholder
@@ -24,6 +40,25 @@ def search_form():
     html_code = flask.render_template('search_form.html')
     response = flask.make_response(html_code)
     return response
+
+# @app.route('/protected')
+# def protected():
+#     # Force CAS authentication (will redirect to CAS if needed)
+#     auth.authenticate()
+#     return f"Hello {auth.get_username()}! This is protected."
+
+@app.route('/api/data')
+def api_data():
+    if not auth.is_authenticated():
+        flask.abort(403)
+    return flask.jsonify({"user": auth.get_username(), "data": []})
+
+@app.route('/', methods=['GET'])
+def landing_page():
+    html_code = flask.render_template('welcome_login.html')
+    response = flask.make_response(html_code)
+    return response
+
 
 if __name__ == "__main__":
     app.run(debug=True)
