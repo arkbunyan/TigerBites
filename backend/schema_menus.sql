12600d829ab857b1ea9ef4709fee5361a1c5b9ce
Ark Bunyan <ab4801@princeton.edu>
2025-12-12 21:20:11 -0500

    removed ignored files

--- backend/schema_menus.sql
+++
@@ -1,9 +0,0 @@
--- menu tables
-CREATE TABLE IF NOT EXISTS menu_items (
-  id INTEGER PRIMARY KEY AUTOINCREMENT,
-  restaurant_id INTEGER NOT NULL,
-  name TEXT NOT NULL,
-  description TEXT,
-  price_cents INTEGER,    
-  UNIQUE(restaurant_id, name)
-);
